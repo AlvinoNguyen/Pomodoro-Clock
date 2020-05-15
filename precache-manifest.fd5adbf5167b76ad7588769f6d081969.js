460ab2217d41877d2f3735acd2312f4a3868c9a1
Alvin Nguyen <alvinonguyen00@gmail.com>
2020-05-15 01:54:42 -0700

    Updates

---
+++ precache-manifest.fd5adbf5167b76ad7588769f6d081969.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "036afaefa27036cb18a11d00eeb5e3b7",
+    "url": "/Pomodoro-Clock/index.html"
+  },
+  {
+    "revision": "fe41a913ab26b3815594",
+    "url": "/Pomodoro-Clock/static/css/main.aa1562fd.chunk.css"
+  },
+  {
+    "revision": "377d5341722ff0b1419b",
+    "url": "/Pomodoro-Clock/static/js/2.2bbe84a0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pomodoro-Clock/static/js/2.2bbe84a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe41a913ab26b3815594",
+    "url": "/Pomodoro-Clock/static/js/main.4066b781.chunk.js"
+  },
+  {
+    "revision": "259b240930c3d7fd2955",
+    "url": "/Pomodoro-Clock/static/js/runtime-main.0e787d0c.js"
+  }
+]);
